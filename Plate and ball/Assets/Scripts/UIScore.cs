eab00072b7fc7cbc06fe623bc88bf421bfde070b
Sergey <b0bkov21@yandex.ru>
2021-11-12 00:51:52 +0300

    Add Score
    -Реализован подсчет очков, за уничтоженные блоки.

--- Plate and ball/Assets/Scripts/UIScore.cs
+++ Plate and ball/Assets/Scripts/UIScore.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 public class UIScore : MonoBehaviour
 {
     
+    private int scoreValue;
+    private Text score;
+    private void Awake()
+    {
+        score = GetComponent<Text>();
+        scoreValue = 0;
+        score.text = scoreValue.ToString();
+    }
+    public void ChangeScore(int pointsForEnemy)
+    {
+        scoreValue += pointsForEnemy;
+        score.text = scoreValue.ToString();
+    }
 }
